b18cb8e8002632b3fba714b89f816a89eeb9f61c
scraaad13579 <56598690+scraaad13579@users.noreply.github.com>
2019-10-21 22:43:21 -0500

    Add files via upload

---
+++ 02_country-capitals.py
@@ -0,0 +1,21 @@
+country = [ 
+            "Brazil", 
+            "Russia", 
+            "India", 
+            "China", 
+            "South Africa"
+          ]
+capitals = {
+    "Brazil": "Brasilia",
+    "Russia": "Moscow",
+    "India": "New Delhi",
+    "China": "Beijing",
+    "South Africa": [
+                        "Pretoria",
+                        "Cape Town",
+                        "Bloemfontein"
+                    ]
+           }
+print("country")
+print("capitals")
+print(capitals["South Africa"][1])
